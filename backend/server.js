b86db4cbb759742d592b639d13404cb013166ae2
Yash Chitmalwar <yashchitmalwar@gmail.com>
2026-03-26 17:45:57 +0530

    server updated

--- backend/server.js
+++ backend/server.js
@@ -33,8 +33,10 @@ app.get('/products', (req, res) => {
 });
 
 // ✅ Server
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log("Server running on port " + PORT);
 });
 
 
